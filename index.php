99e08f5bb8d33530013ac61e162349ca9db12829
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-28 15:10:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Riscrivere questa pagina del sito google:
+https://policies.google.com/faq.
+
+Ci sono diverse domande con relative risposte.
+Realizziamo la struttura dati che ci sembra più adatta per poter poi stampare con un ciclo le domande e risposte della sezione centrale.
+Consigli:
+Teniamo a mente le differenze tra array associativo ed array con indici numerici
+Ricordiamo che possiamo inserire tag all'interno delle stringhe di PHP, saranno convertiti in HTML quando faremo echo
+Concentriamoci sulle FAQ e lasciamo per ultimo header e footer (e il loro CSS)
+Lasciamo per ultimo header e footer (e il loro CSS) e concentriamoci sulle FAQ
+Prima facciamo le FAQ e solo dopo header e footer.
+
+BONUS:
+Inserire le voci di menu dell'header tramite un array di PHP (i link non devono funzionare)
+
+*/
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <title>Google privacy and term</title>
+</head>
+
+<body>
+<div class="container">
+
+</div>
+</body>
+
+</html>
